c8fc7b1e0ad4fbda7dd1a4190d3886b5a12a5a90
posion-br09 <abhay25kumr@gmail.com>
2024-04-03 00:17:05 +0530

    nothing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,15 +9,15 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             lowercase: true,
-            trim: true, 
+            trim: true,
             index: true
         },
         email: {
             type: String,
             required: true,
             unique: true,
-            lowecase: true,
-            trim: true, 
+            lowercase: true,
+            trim: true,
         },
         fullName: {
             type: String,
