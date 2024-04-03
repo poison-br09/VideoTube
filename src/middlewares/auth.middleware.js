b408b45499aff204ba62af67629d2ca6196c1248
posion-br09 <abhay25kumr@gmail.com>
2024-04-04 03:40:40 +0530

    Some minor update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,8 @@ import { User } from "../models/user.model";
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 import jwt from "jsonwebtoken"
-
+// export const verifyJWT = asyncHandler(async(req, _, next)=>{
+    // agar kuch nahi aaye to khali rakhne ke liye shi hai ye 
 export const verifyJWT = asyncHandler(async(req, res, next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
